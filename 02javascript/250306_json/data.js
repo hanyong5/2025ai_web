0a2474ca65e5b4b634f2baa2cea8707c2b751553
hanyong5 <hanyong5@naver.com>
2025-03-06 14:46:13 +0900

    modi

---
+++ 02javascript/250306_json/data.js
@@ -0,0 +1,29 @@
+const han = [
+  {
+    subject: "안녕하세요",
+    name: "한성용",
+    date: "2025-01-20",
+    comment: [{}, {}, {}],
+  },
+  {
+    name: "식빵",
+    family: "웰시코기",
+    age: 1,
+
+    weight: 2.14,
+  },
+  {
+    name: "식빵",
+    family: "웰시코기",
+    age: 1,
+  },
+];
+
+const han1 = ["서울", "부산", "대구"];
+han1[0];
+
+for (var i = 0; i < han1.length; i++) {
+  document.write(han1[i]);
+}
+
+//han[0].comment[] -> 웰시코기
