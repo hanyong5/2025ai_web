b19a6b8271acb0d5f38066f0045647a2894a0163
hanyong5 <hanyong5@naver.com>
2025-03-13 17:22:22 +0900

    todo view

--- 02javascript/250311_todolist/js/script03.js
+++ 02javascript/250311_todolist/js/script03.js
@@ -20,8 +20,12 @@ function viewData() {
   let viewList = ""
   listData.forEach(function (item, index) {
     viewList += `<li>
-                    <div><input type="checkbox" />${item}</div> 
-                    <div>[삭제]</div>
+                    <div>
+                    <input type="checkbox" ${item.completed ? "checked" : ""}  
+                    onclick="toggleCompleted(${index})" />
+                        <span class="${item.completed ? "view" : ""}">${item.text}</span>
+                    </div> 
+                    <div onclick="del(${index})" class="myDel">[삭제]</div>
                 </li>`
   })
 
@@ -32,7 +36,7 @@ function viewData() {
 btn.addEventListener("click", function () {
   let value = inp.value
   if (value) {
-    listData.push(value)
+    listData.push({ text: value, completed: false })
     saveData()
     inp.value = ""
     viewData()
@@ -46,6 +50,19 @@ dBtn.addEventListener("click", function () {
   saveData()
   viewData()
 })
+
+function del(index) {
+  //   alert(index)
+  listData.splice(index, 1)
+  saveData()
+  viewData()
+}
+
+function toggleCompleted(index) {
+  listData[index].completed = !listData[index].completed
+  saveData()
+  viewData()
+}
 
 loadData()
 viewData()
