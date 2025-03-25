96020dcaa7aee8acdbf0fc698bc91c381b2509a2
hanyong5 <hanyong5@naver.com>
2025-03-25 11:28:04 +0900

    pagination#1

--- 02javascript/250324_axios/js/script.js
+++ 02javascript/250324_axios/js/script.js
@@ -1,6 +1,8 @@
 const Url = "https://dapi.kakao.com/v2/search/web"
 const API_KEY = "a87c5485c788a8a30b03b139d3818495"
 let query = ""
+let currentPage = 1
+let totalPage = 1
 
 const inpElm = document.querySelector("#query")
 
@@ -8,10 +10,10 @@ const inpElm = document.querySelector("#query")
 document.querySelector("#searchBtn").addEventListener("click", function () {
   //   alert("test")
   query = inpElm.value.trim()
-  search()
+  search(currentPage)
 })
 
-async function search() {
+async function search(page) {
   // if(query == ""){
   if (!query) {
     alert("검색어를 입력하세요")
@@ -19,7 +21,7 @@ async function search() {
     return
   }
 
-  const apiUrl = Url + "?query=" + query
+  const apiUrl = Url + "?query=" + query + "&page=" + page
   // const apiUrl = `${Url}?query=${query}&sort=accruracy&page=${page}`
   try {
     const response = await axios.get(apiUrl, {
@@ -38,7 +40,28 @@ async function search() {
     })
 
     document.querySelector(".results").innerHTML = kakaoData
+    totalPage = Math.min(50, Math.ceil(data.meta.pageable_count / 10))
+    currentPage = page
+    pagination()
   } catch (error) {
     console.error("에러남" + error)
+  }
+}
+
+function pagination() {
+  const pn = document.querySelector(".pagination")
+  pn.innerHTML = ""
+
+  const startPage = Math.floor((currentPage - 1) / 10) * 10 + 1 // init 1
+  const endPage = Math.min(startPage + 9, totalPage) // init 10
+
+  console.log("page : " + startPage, endPage)
+
+  for (let i = startPage; i <= endPage; i++) {
+    const liElem = document.createElement("li") // <li class=""></li>
+    liElem.className = `page-item ${i == currentPage ? "active" : ""}`
+    liElem.innerHTML = `<a href="" class="page-link">${i}</a>`
+    // liElem.textContent = i
+    pn.appendChild(liElem)
   }
 }
